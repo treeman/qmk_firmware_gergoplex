436b47332d1b574b293d7ec39d770cf4a78bcabe
Jonas Hietala <mail@jonashietala.se>
2020-08-12 21:46:05 +0200

    Numbers layer

--- keyboards/gboards/k/gergoplex/keymaps/hietala-beakl/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/hietala-beakl/keymap.c
@@ -10,6 +10,9 @@
 #include "g/keymap_combo.h"
 
 #define BASE 0 // default layer
+#define SYM 1 // symbol layer
+#define NUM 2 // number layer
+#define FUN 3 // function layer
 
 /* Combomap
  *
@@ -25,15 +28,15 @@
  *  '-------------------------'           '-----------------'
  */
 
-#define Y_MOD MT(MOD_LGUI, KC_Y)
-#define I_MOD MT(MOD_LCTL, KC_I)
-#define E_MOD MT(MOD_LALT, KC_E)
-#define A_MOD MT(MOD_LSFT, KC_A)
+#define Y_MOD LGUI_T(KC_Y)
+#define I_MOD LCTL_T(KC_I)
+#define E_MOD LALT_T(KC_E)
+#define A_MOD LSFT_T(KC_A)
 
-#define S_MOD MT(MOD_RSFT, KC_S)
-#define T_MOD MT(MOD_RALT, KC_T)
-#define N_MOD MT(MOD_RCTL, KC_N)
-#define B_MOD MT(MOD_RGUI, KC_B)
+#define S_MOD RSFT_T(KC_S)
+#define T_MOD RALT_T(KC_T)
+#define N_MOD RCTL_T(KC_N)
+#define B_MOD RGUI_T(KC_B)
 
 // Blank template at the bottom
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,7 +58,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     Y_MOD, I_MOD,  E_MOD,  A_MOD, KC_DOT,     KC_D, S_MOD, T_MOD, N_MOD, B_MOD,
     KC_J,  KC_COLN,KC_SLSH,KC_K,  KC_COMM,    KC_W, KC_M,  KC_L,  KC_P,  KC_V,
 
-    KC_PERC, KC_SPC,    KC_NO,  // Left
-    KC_NO,   KC_RETURN, KC_EQL  // Right
+    KC_PERC,  KC_SPC,    OSL(NUM),  // Left
+    OSL(SYM), KC_RETURN, KC_EQL     // Right
+    ),
+/* Keymap 2: Number layer
+ *
+ * ,-----------------------------.       ,--------------------------------.
+ * |      |     |     |     |     |      |     |     |     |     |        |
+ * |-----+-----+-----+-----+------|      |--------------------------------|
+ * |      |     |     |     |     |      |     |     |     |     |        |
+ * |-----+-----+-----+-----+------+      |--------------------------------|
+ * |      |     |     |     |     |      |     |     |     |     |        |
+ * `------+-----+-----+------+----'        `--------------------------------'
+ *  .-------------------------.           .-----------------.
+ *  |        |       |        |           |        |    |   |
+ *  '-------------------------'           '-----------------'
+ */
+[NUM] = LAYOUT_gergoplex(
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,    KC_NO, KC_7, KC_8, KC_9, KC_NO,
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,    KC_NO, KC_0, KC_1, KC_2, KC_3,
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,    KC_NO, KC_4, KC_5, KC_6, KC_NO,
+
+    KC_TRNS, KC_TRNS, KC_TRNS, // Left
+    KC_TRNS, KC_PENT, KC_TRNS  // Right
     )
 };
