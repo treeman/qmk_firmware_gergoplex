c884369e5a49dd7d8f65668957d3e80c7e707f8a
Jonas Hietala <mail@jonashietala.se>
2020-10-05 20:21:18 +0200

    More poking and proddings

--- keyboards/gboards/k/gergoplex/keymaps/hietala-beakl/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/hietala-beakl/keymap.c
@@ -2,15 +2,10 @@
 
 /*
  * TODO
- * 1. Dead characters don't work properly
- * 2. Modifier support on shortcut layer
- * 3. TAPPING_FORCE_HOLD per key?
- * 4. Move some altgr keys?
- * 5. Highlight altgre keys?
- *
- *
- * Another attempt:
- *
+ * 1. Shortcut layer mod taps
+ * 2. Overlayed mod one-shots
+ * 3. Sometimes pressing / and : in rapid succession makes shift/altgr to get stuck
+ *    (possibly other SE symbols too).
  *
  * Experiences with home row mods:
  *   https://www.reddit.com/r/ErgoMechKeyboards/comments/ibsi0k/your_experiences_with_modlayer_tap/
@@ -20,22 +15,19 @@
 #include "keymap_swedish.h"
 #include "sendstring_swedish.h"
 
+#define BASE 0
+#undef UP
+#define UP 1
+#define FUN 2
+#define MOVE 3
+#define SHRT 4
+#define GAME 5
+
 #define _______ KC_TRNS
 #define xxxxx KC_NO
 
-#define HOME_Y LGUI_T(SE_Y)
-#define HOME_I LALT_T(SE_I)
-#define HOME_E LSFT_T(SE_E)
-#define HOME_A LCTL_T(SE_A)
-
-// Right shift adds a return for some reason :<
-#define HOME_S RCTL_T(SE_S)
-#define HOME_T LSFT_T(SE_T)
-#define HOME_N RALT_T(SE_N)
-#define HOME_B RGUI_T(SE_B)
-
-#define TH_SPACE KC_SPACE
-#define TH_ENTER KC_ENT
+#define TH_SPACE LT(MOVE, KC_SPACE)
+#define TH_ALT OSM(MOD_LALT)
 
 enum my_keycodes {
   MOVE_ON = SAFE_RANGE,
@@ -46,30 +38,8 @@ enum my_keycodes {
   TILD,
 };
 
-#define BASE 0 // default layer
-#define SYM 1 // symbol layer
-#define NUM 2 // number layer
-#define FUN 3 // function layer
-#define MOVE 4 // movement/mouse layer
-#define SHRT 5 // shortcut layer
-
 #include "g/keymap_combo.h"
 
-/* Combomap
- *
- * ,-----------------------------.       ,--------------------------------.
- * |      |    ESC    |     |     |      |     |    ESC    |    BSLH      |
- * |-----+-----+-----+-----+------|      |--------------------------------|
- * |      |   BSPC   ENT    |     |      |    LES   COLN  GRT    |        |
- * |-----+-----+-----+--RMB+-LMB--+      |--------------------------------|
- * |      |   MINS    |     |     |      |    QUO   UNDR   |     |        |
- * `------+-----+-----+------+----'      `--------------------------------'
- *  .-------------------------.           .-----------------.
- *  |        |       |        |           |        |    |   |
- *  '-------------------------'           '-----------------'
- */
-
-// Blank template at the bottom
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -85,65 +55,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-------------------------'           '-----------------'
  */
 [BASE] = LAYOUT_gergoplex(
-    SE_Q,  SE_H,   SE_O,   SE_U,  SE_X,       SE_G, SE_C,  SE_R,  SE_F,  SE_Z,
-    HOME_Y,HOME_I, HOME_E, HOME_A,SE_DOT,     SE_D, HOME_S,HOME_T,HOME_N,HOME_B,
-    SE_J,  SE_COLN,SE_SLSH,SE_K,  SE_COMM,    SE_W, SE_M,  SE_L,  SE_P,  SE_V,
+    KC_LEAD, SE_H,   SE_O,    SE_U, SE_Q,     SE_G, SE_C, SE_R, SE_F, SE_Z,
+    SE_Y,    SE_I,   SE_E,    SE_A, SE_COLN,  SE_D, SE_S, SE_T, SE_N, SE_B,
+    KC_J,    SE_DOT, SE_COMM, SE_K, SE_X,     SE_W, SE_M, SE_L, SE_P, SE_V,
 
-    OSL(SHRT), TH_SPACE,  OSL(NUM),  // Left
-    OSL(SYM),  TH_ENTER,  KC_LEAD    // Right
+    TH_ALT,  TH_SPACE,      OSM(MOD_LCTL),
+    OSL(UP), OSM(MOD_LSFT), OSM(MOD_LGUI)
     ),
-/* Keymap 1: Symbols layer
+/* Keymap 1: Up layer
  */
-[SYM] = LAYOUT_gergoplex(
-    xxxxx, SE_AT,   SE_PLUS, SE_PIPE, xxxxx,    _______,_______,_______,_______,_______,
-    xxxxx, TILD,    SE_EXLM, SE_QUES, GRV,      SE_TILD,SE_GRV, SE_CIRC,SE_ACUT,SE_DIAE,
-    xxxxx, SE_AMPR, CIRC,    SE_BSLS, xxxxx,    _______,_______,_______,_______,_______,
+[UP] = LAYOUT_gergoplex(
+    SE_TILD,SE_GRV, SE_CIRC,SE_ACUT,SE_DIAE,    xxxxx, SE_7, SE_8, SE_9, xxxxx,
+    _______,SE_ODIA,SE_ADIA,SE_ARNG,_______,    xxxxx, SE_1, SE_2, SE_3, xxxxx,
+    _______,_______,_______,_______,OSL(FUN),   xxxxx, SE_4, SE_5, SE_6, xxxxx,
 
-    _______, _______, _______, // Left
-    _______, _______, _______  // Right
+    _______, SE_0,    _______,
+    _______, _______, _______
     ),
-/* Keymap 2: Number layer
- *
- * ,-----------------------------.       ,--------------------------------.
- * |      |     |     |     |     |      |     |     |     |     |        |
- * |-----+-----+-----+-----+------|      |--------------------------------|
- * |      |     |     |     |     |      |     |     |     |     |        |
- * |-----+-----+-----+-----+------+      |--------------------------------|
- * |      |     |     |     |     |      |     |     |     |     |        |
- * `------+-----+-----+------+----'        `--------------------------------'
- *  .-------------------------.           .-----------------.
- *  |        |       |        |           |        |    |   |
- *  '-------------------------'           '-----------------'
- */
-[NUM] = LAYOUT_gergoplex(
-    _______, _______,_______,_______,_______,    xxxxx, SE_7, SE_8, SE_9, xxxxx,
-    _______, _______,_______,_______,_______,    xxxxx, SE_0, SE_1, SE_2, SE_3,
-    OSL(FUN),_______,_______,_______,_______,    xxxxx, SE_4, SE_5, SE_6, xxxxx,
-
-    _______, _______,         _______, // Left
-    _______, RGUI_T(KC_PENT), _______  // Right
-    ),
-/* Keymap 3: Function layer
+/* Keymap 2: Function layer
  */
 [FUN] = LAYOUT_gergoplex(
-    _______,_______,_______,_______,_______,    KC_F12, KC_F7,  KC_F8, KC_F9, xxxxx,
-    _______,_______,_______,_______,_______,    KC_F11, KC_F10, KC_F1, KC_F2, KC_F3,
-    _______,_______,_______,_______,_______,    xxxxx,  KC_F4,  KC_F5, KC_F6, xxxxx,
+    _______,_______,_______,_______,_______,    KC_F12, KC_F7, KC_F8, KC_F9, xxxxx,
+    _______,_______,_______,_______,_______,    KC_F11, KC_F1, KC_F2, KC_F3, xxxxx,
+    _______,_______,_______,_______,_______,    xxxxx,  KC_F4, KC_F5, KC_F6, xxxxx,
 
     _______, _______, _______, // Left
-    _______, _______, _______  // Right
+    _______, KC_F10,  _______  // Right
     ),
-/* Keymap 4: Movement/mouse layer
+/* Keymap 3: Movement/mouse layer
  */
 [MOVE] = LAYOUT_gergoplex(
     KC_END, KC_PGDN,KC_UP,  KC_PGUP, KC_HOME,    _______,_______,_______,_______,_______,
     KC_BTN4,KC_LEFT,KC_DOWN,KC_RIGHT,KC_BTN5,    _______,_______,_______,_______,_______,
-    xxxxx,  KC_BTN1,_______,KC_BTN2, xxxxx,      _______,_______,_______,_______,_______,
+    xxxxx,  KC_BTN1,KC_BTN3,KC_BTN2, xxxxx,      _______,_______,_______,_______,_______,
 
-    OSL(SHRT), KC_SPACE, KC_ENTER, // Left
+    _______, _______, _______, // Left
     _______,   _______,  _______   // Right
     ),
-/* Keymap 5: Shortcut layer
+/* Keymap 4: Shortcut layer
  */
 [SHRT] = LAYOUT_gergoplex(
     SE_Q,SE_W,SE_E,SE_R,SE_T,    _______,_______,_______,_______,_______,
@@ -156,6 +105,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool swap_caps_escape = false;
+bool remapping_shift = false;
+
+bool mod_down(uint16_t mod) {
+    return keyboard_report->mods & MOD_BIT(mod);
+}
+
+bool remap_shift(keyrecord_t *record, uint16_t keycode) {
+    if (record->event.pressed) {
+        bool shift = keyboard_report->mods & MOD_BIT(KC_LSFT);
+        bool one_shot_shift = (get_oneshot_mods() & MOD_BIT(KC_LSFT)) &&
+            !has_oneshot_mods_timed_out() && false;
+
+        if (!shift && !one_shot_shift) return false;
+
+        if (shift) {
+            unregister_mods(MOD_BIT(KC_LSFT));
+            tap_code16(keycode);
+            register_mods(MOD_BIT(KC_LSFT));
+        } else {
+            clear_oneshot_mods();
+            tap_code16(keycode);
+        }
+        remapping_shift = true;
+    } else {
+        if (!remapping_shift) return false;
+        // If we move unregister_code and register shift here,
+        // and if we do
+        //
+        //  shift down
+        //  keycode down
+        //  shift up
+        //  keycode up
+        //
+        // we'll get stuck with shift.
+        //
+        // If we instead tap on pressed auto repeat breaks, but
+        // we'll never find ourselves in a bad position.
+        remapping_shift = false;
+    }
+    return true;
+}
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -194,6 +185,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case TILD:
       SEND_STRING("~");
       return false;
+    case SE_DOT:
+      // . -> !
+      if (remap_shift(record, SE_EXLM)) return false;
+      return true;
+    case SE_COMM:
+      // , -> ?
+      if (remap_shift(record, SE_QUES)) return false;
+      return true;
+    case SE_SLSH:
+      // / -> \ (unshift + altgr)
+      if (remap_shift(record, SE_BSLS)) return false;
+      return true;
+    case SE_COLN:
+      // : -> @ (unshift + altgr)
+      if (remap_shift(record, SE_AT)) return false;
+      return true;
     default:
       return true;
   }
@@ -208,9 +215,13 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(TH_ENTER) {
-      tap_code(KC_LGUI);
+    SEQ_ONE_KEY(SE_E) {
+      SEND_STRING("€");
     }
+    SEQ_ONE_KEY(SE_C) {
+      SEND_STRING("©");
+    }
+
     SEQ_TWO_KEYS(SE_C, SE_C) {
       if (swap_caps_escape) {
         tap_code(KC_ESC);
@@ -218,7 +229,8 @@ void matrix_scan_user(void) {
         tap_code(KC_CAPS);
       }
     }
-    SEQ_TWO_KEYS(SE_S, SE_C) {
+
+    SEQ_ONE_KEY(KC_ESC) {
       swap_caps_escape = !swap_caps_escape;
     }
     SEQ_TWO_KEYS(SE_P, SE_V) {
@@ -229,13 +241,4 @@ void matrix_scan_user(void) {
       send_string(wpm_str);
     }
   }
-}
-
-// FIXME this wasn't good enough :(
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-    if (combo->keys[0] == TH_ENTER) {
-        return 100;
-    }
-
-    return COMBO_TERM;
 }
